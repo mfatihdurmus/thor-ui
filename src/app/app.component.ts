4be389a2ce3e616939d55cf994fbca36f9c217d2
mfatihdurmus <fatihdurmus@live.com>
2020-02-12 00:14:29 +0300

    angular testler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thor-ui';
+  todos = [
+    { 
+      id: 1,
+      text: 'Servisler (Process task create read update)'
+    },
+    { id: 2,
+      text: 'JWT ile login'
+    },
+    {
+      id : 5,
+      text: 'angularda sayfaların oluşturulması'
+    }
+  ];
+
+  onSelect(item): void {
+    alert(item.id);
+  }
 }
